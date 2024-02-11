0c03acc47a52aa23856341ceeac4ddee35aaeb17
Michalenok Marina <mihalenokee@outlook.com>
2024-02-11 11:22:00 +0300

    create GH actions based CI pipeline

---
+++ src/test/java/com/company/rentstuff/RentStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.rentstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
